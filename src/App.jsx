cc9aa9ec83675026adc98829015244df9ae42323
Heath114 <heathens1143@gmail.com>
2025-06-27 19:51:57 +0300

    Implement the about section

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import gsap from 'gsap';
 import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx'
 import Cocktails from './components/Cocktails.jsx';
-
+import About from './components/About.jsx';
 
 
 
@@ -17,6 +17,7 @@ const App = () => {
       <Navbar />
       <Hero /> 
       <Cocktails />
+      <About />
 
     </main >
   );
